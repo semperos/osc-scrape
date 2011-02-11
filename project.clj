37994f370e1838353bea401d70fea12b64c00433
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-02-11 15:31:43 -0500

    Add dev dependencies for evalive and swank-clojure

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [enlive "1.0.0-SNAPSHOT"]
 		 [hiccup "0.3.4"]]
-  :dev-dependencies [[marginalia "0.5.0-alpha"]]
+  :dev-dependencies [[marginalia "0.5.0-alpha"]
+		     [evalive "1.0.0"]
+		     [swank-clojure "1.3.0-SNAPSHOT"]]
   :main osc-scrape.core)
