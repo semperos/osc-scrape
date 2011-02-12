c4a1fd4cb643d6b2ba0cd8980f9b39c2b8c89c45
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-02-12 04:13:13 -0500

    Initial tests of osc-scrape.core

--- test/osc_scrape/test/core.clj
+++ test/osc_scrape/test/core.clj
@@ -1,6 +1,20 @@
 (ns osc-scrape.test.core
-  (:use [osc-scrape.core] :reload)
+  (:use [osc-scrape core util] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest get-page-title
+  (is (= "OSC Scrape" (select-page-title
+		       {:tag :html, :attrs nil, :content[
+							 {:tag :head, :attrs nil, :content [
+											    {:tag :title, :attrs nil, :content ["OSC Scrape"]}]}
+							 {:tag :body, :attrs nil, :content nil}]}))))
+
+(deftest make-full-url
+  (are [part full] (= full (build-full-url "http://www.oilspillcommission.gov/resources" part))
+       "/sites/default/files"                       "http://www.oilspillcommission.gov/sites/default/files"
+       "/news#media-advisories"                     "http://www.oilspillcommission.gov/news"
+       "docs/document.pdf"                          "http://www.oilspillcommission.gov/resources/docs/document.pdf"
+       "http://www.oilspillcommission.gov/"         "http://www.oilspillcommission.gov/"
+       "http://www.oilspillcommission.gov#foobar"   "http://www.oilspillcommission.gov/"))
+       
+		     
