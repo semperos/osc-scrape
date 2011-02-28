8c06e96d295ac19b9a31bd4de34183525c19bfc5
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-02-27 22:56:08 -0500

    Needs better test coverage, but establishing a version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject osc-scrape "1.0.0-SNAPSHOT"
+(defproject osc-scrape "0.1.0"
   :description "Code for Scraping OSC Website"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
