2b32ee7570c57e3c3f892489a5980eba6c8d1e46
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-02-28 23:36:30 -0500

    Use radiobuttons, add a progress bar and change status bar text

--- src/osc_scrape/gui.clj
+++ src/osc_scrape/gui.clj
@@ -2,8 +2,8 @@
   (:use [osc-scrape core util])
   (:import [javax.swing
             JFrame JPanel JButton
-            JCheckBox JLabel
-            JOptionPane SwingWorker]
+            JRadioButton ButtonGroup JLabel
+            JOptionPane JProgressBar SwingWorker]
            net.miginfocom.swing.MigLayout)
   (:gen-class
    :main true))
@@ -16,40 +16,54 @@
         (actionPerformed [~event] ~@body))))
 
 ;;; Component Definitions
-(def btn-run (JButton. "Run Scrape"))
-(def chkbox-scrape (doto (JCheckBox. "Scrape")
-                      (.setSelected true)))
-(def chkbox-scrape-and-dl (JCheckBox. "Scrape & Download"))
-(def lbl-status (JLabel. "Status: (Not Running)"))
+(def btn-run (JButton. "Run!"))
+(def radio-btn-scrape (JRadioButton. "Scrape" true))
+(def radio-btn-scrape-and-dl (JRadioButton. "Scrape & Download"))
+(def btn-group (doto (ButtonGroup.)
+                 (.add radio-btn-scrape)
+                 (.add radio-btn-scrape-and-dl)))
+(def lbl-status (JLabel. "Status: Not Running"))
+(def pb (doto (JProgressBar. 0 1)
+          (.setStringPainted false)
+          (.setValue 0)))
 
 ;;; Actions
 (on-action btn-run event
-           (let [statuses (map #(.isSelected %) [chkbox-scrape chkbox-scrape-and-dl])]
-             (if (every? false? statuses)
-               (JOptionPane/showMessageDialog
-                nil
-                "You must select at least one scrape option before running the scrape"
-                "Scrape Options"
-                JOptionPane/ERROR_MESSAGE)
-               (let [sw (doto (proxy [SwingWorker] []
-                       (doInBackground []
-                         (.setText lbl-status "Status: RUNNING SCRAPE, please be patient.")
-                         (if (true? (.isSelected chkbox-scrape-and-dl))
-                           (start-scrape outline scrape-urls true)
-                           (start-scrape outline scrape-urls false)))
-                       (done []
-                         (.setText lbl-status "Status: COMPLETE"))))]
-                 (.execute sw)))))
+           (let [start-scrape-and-dl (.isSelected radio-btn-scrape-and-dl)]
+             (if (true? start-scrape-and-dl)
+               (.setText lbl-status "RUNNING SCRAPE & DOWNLOAD, please be very patient.")
+               (.setText lbl-status "RUNNING SCRAPE, please be patient."))
+             (.setValue pb 0)
+             (.setIndeterminate pb true)
+             (.setEnabled btn-run false)
+             (.setEnabled radio-btn-scrape false)
+             (.setEnabled radio-btn-scrape-and-dl false)
+             (doto (proxy [SwingWorker] []
+                            (doInBackground []
+                              (if (true? start-scrape-and-dl)
+                                (start-scrape true)
+                                (start-scrape false)))
+                            (done []
+                              (.setIndeterminate pb false)
+                              (.setValue pb 1)
+                              (.setEnabled btn-run true)
+                              (.setEnabled radio-btn-scrape true)
+                              (.setEnabled radio-btn-scrape-and-dl true)
+                              (.setText lbl-status "Status: COMPLETE")))
+               (.execute))))
 
 ;;; Layout
 (def layout (MigLayout. "wrap 3", "[][]"))
 (def pnl-main (doto (JPanel. layout)
                 (.add btn-run)
-                (.add chkbox-scrape)
-                (.add chkbox-scrape-and-dl)
+                (.add radio-btn-scrape)
+                (.add radio-btn-scrape-and-dl)
+                (.add pb "span 3, growx, gaptop 15")
                 (.add lbl-status "span 3, growx, gaptop 15")))
+
 (def frame (doto (JFrame. "OSC Site Scraper")
-             (.setSize 370 130)
+             (.setSize 370 150)
+             (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
              (.setContentPane pnl-main)))
 
 (defn- create-and-show-gui []
